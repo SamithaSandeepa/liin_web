eb6b021e038fb17e971ba45e7081015c5c3aef61
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2025-12-10 12:06:45 +0530

    Update AdvertisementModal.tsx

--- src/components/ui/AdvertisementModal.tsx
+++ src/components/ui/AdvertisementModal.tsx
@@ -154,7 +154,7 @@ export default function AdvertisementModal({
                      alt={currentAd.title || "Advertisement"}
                      width={800}
                      height={600}
-                     className="w-auto h-auto max-h-[85vh] md:max-h-[85vh] object-contain block"
+                     className="w-auto h-auto max-h-[95vh] md:max-h-[85vh] object-contain block"
                      priority
                    />
                   )}
