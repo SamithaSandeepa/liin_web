f4ab636e7ab241210db57a4fc301e8e02b7af3cc
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2025-12-12 23:25:46 +0530

    up

--- src/app/projects/climate-challenge/page.tsx
+++ src/app/projects/climate-challenge/page.tsx
@@ -94,7 +94,7 @@ export default function ClimateChallengePage() {
       <HeroSection
         title="Climate Challenge SL"
         subtitle="Creative Data Solutions for Resilience"
-        backgroundImage="/images/projects/climate_challenge/cover.png"
+        backgroundImage="/images/projects/climate_challenge/cover.jpg"
         backgroundSize="contain"
       />
 
