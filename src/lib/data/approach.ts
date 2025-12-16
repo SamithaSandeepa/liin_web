bdb737b1ed8b94e452dde2377adcd9034c1e850b
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2025-12-16 16:53:46 +0530

    up

--- src/lib/data/approach.ts
+++ src/lib/data/approach.ts
@@ -5,19 +5,19 @@ export const approachData: ApproachCard[] = [
     img: "/images/ourapproach/capital-mobilization.jpg",
     title: "Capital Mobilization",
     desc: "Through a rigorous capital mobilization strategy, we identify high-potential impact ventures and align them with suitable investors, ensuring strategic funding that drives measurable and long-term development outcomes.",
-    link: "/partners", // Impact Fund page
+    link: "/impact-funds/ief", // Impact Fund page
   },
   {
     img: "/images/ourapproach/impact.png",
     title: "Impact Investment Advisory & Support",
     desc: "Our advisory team delivers deep local insight and strategic expertise to structure, execute, and manage impact investments in Sri Lanka. We provide the clarity and confidence you need to deploy capital effectively, ensuring your investments achieve meaningful and measurable outcomes.",
-    link: "/about", // Impact Investment Page
+    link: "/investments/philosophy", // Impact Investment Page
   },
   {
     img: "/images/ourapproach/business.jpeg",
     title: "Business Development & Capacity Building",
     desc: "We go beyond investment to build resilient, scalable businesses from the ground up. Our Business Development & Capacity Building services provide entrepreneurs with the essential tools for long-term success.",
-    link: "/projects/pif", // Projects page
+    link: "https://www.liin.lk/grit/", // Projects page
   },
   {
     img: "/images/ourapproach/eco.JPG",
